e52fa8a78046ba59c02f2181ae7eb3a56769b828
David Carey (04003163) <careydt1@student.op.ac.nz>
2017-10-24 14:33:45 -0700

    Make height-map cyclic / remove deadzone

--- PGE/PGE/MapGenerator.cs
+++ PGE/PGE/MapGenerator.cs
@@ -11,6 +11,13 @@ namespace PGE
     /// </summary>
     class MapGenerator
     {
+        private const int shoreAltitude = 0;
+        private const int swampAltitude = 10;
+        private const int palmtreeAltitude = 15;
+        private const int grassAltitude = 20;
+        private const int treeAltitude = 30;
+        private const int mountainAltitude = 50;
+
         /// <summary>
         /// 
         /// </summary>
@@ -22,10 +29,10 @@ namespace PGE
         private static int AverageDiamond(int[,] heightMap, int x, int y, int stepSize)
         {
             int halfStep = stepSize / 2;
-            int N = heightMap[x, y - halfStep];
-            int E = heightMap[x + halfStep, y];
-            int S = heightMap[x, y + halfStep];
-            int W = heightMap[x - halfStep, y];
+            int N = cyclic(heightMap, x, y - halfStep);
+            int E = cyclic(heightMap, x + halfStep, y);
+            int S = cyclic(heightMap, x, y + halfStep);
+            int W = cyclic(heightMap, x - halfStep, y);
             int average = (N + E + S + W) / 4;
             return average;
         }
@@ -40,41 +47,92 @@ namespace PGE
         private static int AverageSquare(int[,] heightMap, int x, int y, int stepSize)
         {
             int halfStep = stepSize / 2;
-            int NW = heightMap[x - halfStep, y - halfStep];
-            int SE = heightMap[x + halfStep, y + halfStep];
-            int SW = heightMap[x - halfStep, y + halfStep];
-            int NE = heightMap[x + halfStep, y - halfStep];
+            int NW = cyclic(heightMap, x - halfStep, y - halfStep);
+            int SE = cyclic(heightMap, x + halfStep, y + halfStep);
+            int SW = cyclic(heightMap, x - halfStep, y + halfStep);
+            int NE = cyclic(heightMap, x + halfStep, y - halfStep);
             int average = (NE + SE + SW + NW) / 4;
             return average;
+        }
+
+        private static int cyclic(int[,] heightMap, int x, int y)
+        {
+            if (y > 1024)
+            {
+                y -= 1024;
+            }
+
+            if (y < 0)
+            {
+                y += 1024;
+            }
+
+            if (x > 1024)
+            {
+                x -= 1024;
+            }
+
+            if (x < 0)
+            {
+                x += 1024;
+            }
+
+            int value = heightMap[y, x];
+            return value;
+        }
+
+        private static void cyclic(int[,] heightMap, int x, int y, int v)
+        {
+            if (y > 1024)
+            {
+                y -= 1024;
+            }
+
+            if (y < 0)
+            {
+                y += 1024;
+            }
+
+            if (x > 1024)
+            {
+                x -= 1024;
+            }
+
+            if (x < 0)
+            {
+                x += 1024;
+            }
+
+            heightMap[y, x] = v;
         }
 
         private static int[,] DiamondSquare(Random r, int[,] heightMap, int stepSize, double scale, int mapWidth, int mapHeight)
         {
             int halfStep = stepSize / 2;
-            int maxXTile = mapWidth - 16;
-            int maxYTile = mapHeight - 12;
+            int maxXTile = mapWidth;
+            int maxYTile = mapHeight;
 
-            for (int x = 16 + halfStep; x < (maxXTile + halfStep); x += stepSize)
+            for (int x = halfStep; x < (maxXTile + halfStep); x += stepSize)
             {
-                for (int y = 12 + halfStep; y < (maxYTile + halfStep); y += stepSize)
+                for (int y = halfStep; y < (maxYTile + halfStep); y += stepSize)
                 {
                     int square = AverageSquare(heightMap, x, y, stepSize);
                     square += Convert.ToInt32(r.Next(0, 100) * scale);
-                    heightMap[x, y] = square;
+                    cyclic(heightMap, y, x, square);
                 }
             }
 
-            for (int x = 16; x < maxXTile; x += stepSize)
+            for (int x = 0; x < maxXTile; x += stepSize)
             {
-                for (int y = 12; y < maxYTile; y += stepSize)
+                for (int y = 0; y < maxYTile; y += stepSize)
                 {
                     int diamondOne = AverageDiamond(heightMap, x + halfStep, y, stepSize);
                     diamondOne += Convert.ToInt32(r.Next(0, 100) * scale);
-                    heightMap[x + halfStep, y] = diamondOne;
+                    cyclic(heightMap, x + halfStep, y, diamondOne);
 
                     int diamondTwo = AverageDiamond(heightMap, x, y + halfStep, stepSize);
                     diamondTwo += Convert.ToInt32(r.Next(0, 100) * scale);
-                    heightMap[x, y + halfStep] = diamondTwo;
+                    cyclic(heightMap, x, y + halfStep, diamondTwo);
                 }
             }
 
@@ -93,13 +151,13 @@ namespace PGE
             int[,] heightMap = new int[mapHeight, mapWidth];
             int[,] map = new int[mapHeight, mapWidth];
 
-            for (int k = 0; k < 1; k++)
+            for (int k = 0; k < 8; k++)
             {
                 // Must be a power of 2:
-                int stepSize = 16;
+                int stepSize = 32;
 
                 // Noise-coefficient: 
-                double scale = 0.42;
+                double scale = 0.337;
 
                 while (stepSize > 1)
                 {
@@ -114,24 +172,24 @@ namespace PGE
                     for (int i = 0; i < mapWidth; i++)
                     {
                         int tileAltitude = heightMap[j, i];
-                        int tileType = 0;
+                        int tileType = 1; // Sea
 
-                        if (tileAltitude >= 10)
-                            tileType = 1; // Sea.
-
-                        if (tileAltitude >= 15)
+                        if (tileAltitude >= shoreAltitude)
                             tileType = 2; // Shore.
 
-                        if (tileAltitude >= 20)
+                        if (tileAltitude >= swampAltitude)
                             tileType = 10; // Swamp.
 
-                        if (tileAltitude >= 25)
+                        if (tileAltitude >= palmtreeAltitude)
+                            tileType = 11;
+
+                        if (tileAltitude >= grassAltitude)
                             tileType = 20; // Grass.
 
-                        if (tileAltitude >= 30)
+                        if (tileAltitude >= treeAltitude)
                             tileType = 30;
 
-                        if (tileAltitude >= 40)
+                        if (tileAltitude >= mountainAltitude)
                             tileType = 40;
 
                         map[j, i] = tileType;
